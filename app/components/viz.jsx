b5bb7d97ba8baf488bf8e1e0bf00a661a2fe2179
Jason Axelson <jason.axelson@gmail.com>
2015-09-13 01:03:13 -1000

    parse rds data (more space efficient than json)

--- app/components/viz.jsx
+++ app/components/viz.jsx
@@ -13,16 +13,41 @@ let Viz = React.createClass({
   },
 
   componentDidMount() {
-    let url = 'http://waterservices.usgs.gov/nwis/iv/?format=json&indent=on&sites=16241600&period=P1D&parameterCd=00065';
-    $.get(url, (result) => {
-      let values = result.value.timeSeries[0].values[0].value;
-      let currentStreamHeight = values.splice(-1)[0].value;
-      if (this.isMounted()) {
-        this.setState({
-          currentStreamHeight,
-        });
-      }
+    let promise = true ? this._getRealData() : this._getMockData();
+    promise.then(this._handleData);
+  },
+
+  _getMockData() {
+    let mockData = 8;
+    return new Promise(function(resolve, reject) {
+      resolve(mockData);
     });
+  },
+
+  _getRealData() {
+    // parameters:
+    // * period: how long (P1D is one day). If not included returns the instantaneous result
+    // * parameterCd: what parameter (stream height, flow)
+    // * sites: the site/sensor to get the data from
+    let url = 'http://waterservices.usgs.gov/nwis/iv/?format=rdb&sites=16241600&parameterCd=00065';
+    return new Promise(function(resolve, reject) {
+      $.get(url).success(function(data) {
+        let lines = data.split("\n").filter( (d) => !d.startsWith('#') );
+        // Remove precision data (what makes this RDS)
+        lines.splice(1, 1);
+        var result = d3.tsv.parse(lines.join("\n"));
+        // 02_00065 has the actual data
+        resolve(result[0]['02_00065']);
+      });
+    });
+  },
+
+  _handleData(currentStreamHeight) {
+    if (this.isMounted()) {
+      this.setState({
+        currentStreamHeight,
+      });
+    }
   },
 
   render() {
